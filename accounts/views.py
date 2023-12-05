99e29a260bccd53fcd3ceaf2a22854473e3dbc26
chidee <chidiebereohiri@gmail.com>
2023-12-05 10:04:53 +0100

    setup account app

---
+++ accounts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .form import * 
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created please log in')
+            return redirect('login')
+        else:
+            messages.warning(request, 'Something went wrong. Please check form errors')
+            return redirect('register')
+    else:
+        form = RegisterUserForm()
+        context = {'form':form}
+    return render(request, 'accounts/register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(request, user)
+        else:
+            messages.warning(request, 'Something went wrong. Please check form errors')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Active session has ended. Log in again')
+    return redirect('login')
